7146be48b15814dfa6372b877bc78e470f5ef224
Ivan Cvitković <icvitkovic@combis.ba>
2020-03-31 16:37:43 +0200

    added user signup

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const ordersRoutes = require("./api/routes/orders");
+const userRoutes = require("./api/routes/users");
 
 mongoose.connect(
   `mongodb+srv://icvitkovic:${process.env.MONGO_ATLAS_PW}@node-rest-api-qjfm5.azure.mongodb.net/test?retryWrites=true&w=majority`,
   {
+    useNewUrlParser: true,
     useUnifiedTopology: true
   }
 );
@@ -34,6 +36,7 @@ app.use((req, res, next) => {
 
 app.use("/products", productRoutes);
 app.use("/orders", ordersRoutes);
+app.use("/user", userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
